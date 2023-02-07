8ddf0e9e7d7cf33100417e40d0293893bd1d2634
meija1 <meija911@gmail.com>
2023-02-07 20:09:16 +0000

    Add valdation function for exactly six values

--- run.py
+++ run.py
@@ -12,13 +12,22 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwitches')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user
-
     """
-
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    try:
+        if len(values) != 6:
+            raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 get_sales_data()
