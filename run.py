a3ad4451bb2b819c83e551e4d6b47b8dedd4ebba
meija1 <meija911@gmail.com>
2023-02-07 20:15:04 +0000

    Validate that data provided is converted to integers

--- run.py
+++ run.py
@@ -25,9 +25,11 @@ def get_sales_data():
 
 def validate_data(values):
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+
 
 get_sales_data()
