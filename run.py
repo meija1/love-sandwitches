7c004b43d4b61f8b2957ae1b2571cd8499e0f5b6
meija1 <meija911@gmail.com>
2023-02-07 21:53:28 +0000

    Collect sales data for last 5 entries in list columns

--- run.py
+++ run.py
@@ -61,6 +61,15 @@ def update_worksheet(data, worksheet):
     print(f"{worksheet} worksheet updated successfully\n")
 
 
+def get_last_5_entries_sales():
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+
 def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
